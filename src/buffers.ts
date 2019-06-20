c02ed1b6490d69e9353ec1004cae30c435a54ccd
Alan Shaw <alan@nodename.com>
2019-06-19 20:59:11 -0400

    removed singleton buffer list

--- src/buffers.ts
+++ src/buffers.ts
@@ -1,7 +1,5 @@
 import * as vscode from "vscode";
 
-let buffers: Buffer[] = [];
-
 type WithPosition = {
   position: number;
 };
@@ -36,29 +34,4 @@ export function isStopPoint(buffer: Buffer): buffer is StopPoint {
   return (
     (<StopPoint>buffer).stop !== undefined && (<StopPoint>buffer).stop !== null
   );
-}
-
-export function all() {
-  return buffers;
-}
-
-export function get(position: number) {
-  return buffers[position];
-}
-
-// @TODO LOL delete this shit
-export function inject(_buffers: Buffer[]) {
-  buffers = _buffers;
-}
-
-export function insert(buffer: Buffer) {
-  buffers.push(buffer);
-}
-
-export function clear() {
-  buffers = [];
-}
-
-export function count() {
-  return buffers.length;
 }
