7b5af2478ef44aa66593b62565f9105657ebb7f4
Alan Shaw <alan@nodename.com>
2019-07-04 18:44:24 -0700

    Added sound player

---
+++ src/sound.ts
@@ -0,0 +1,42 @@
+import * as mm from 'music-metadata';
+
+const wavPlayer = require("node-wav-player");
+const path = require("path");
+
+const state = { isPlaying: false };
+
+export async function playSound() {
+    let soundFileName = "error.wav";
+    let soundFilePath = path.join(
+        __dirname,
+        "..",
+        "sounds",
+        soundFileName
+    );
+
+    let duration: number | undefined;
+    await mm.parseFile(soundFilePath)
+        .then(metadata => {
+            duration = metadata.format.duration;
+        })
+        .catch((err) => {
+            console.error(err.message);
+        });
+
+    state.isPlaying = true;
+    wavPlayer
+        .play({
+            path: soundFilePath,
+            sync: true
+        })
+        .then(() => {
+            state.isPlaying = false;
+        })
+        .catch((error: any) => {
+            console.error(error);
+            state.isPlaying = true;
+        });
+    return new Promise(resolve => {
+        resolve(duration);
+    });
+}
