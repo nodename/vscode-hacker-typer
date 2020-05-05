86290cba90885fecf27ba99a24e2e0276f914e57
Alan Shaw <alan@nodename.com>
2020-05-05 17:33:26 -0400

    No more implicit stop point at end of play

--- src/play.ts
+++ src/play.ts
@@ -17,7 +17,8 @@ import * as statusBar from "./statusBar";
 //     The playChannel is created from the macro's buffers.
 //
 //     The runPlay function consumes the playChannel and the commandChannel
-//     and applies buffers from the playChannel as edits to the document as directed by the commandChannel commands.
+//     and applies buffers from the playChannel (as edits to the document)
+//     as directed by the commandChannel commands.
 //
 //     In autoPlay mode, the onType command puts autoplay commands on the autoPlayControlChannel.
 //     The runAutoPlay function consumes these commands and outputs commands on the commandChannel.
@@ -61,6 +62,10 @@ function setManualKeyboardMode() {
   typeCommand = registerTypeCommand();
 }
 
+function setAutoKeyboardMode() {
+  typeCommand = registerAutoTypeCommand();
+}
+
 function registerTypeCommand() {
   // "type" is a built-in command, so we don't configure a keyboard shortcut.
   // We install the  handler:
@@ -89,10 +94,6 @@ function registerTypeCommand() {
   return vscode.commands.registerCommand("type", onType);
 }
 
-function setAutoKeyboardMode() {
-  typeCommand = registerAutoTypeCommand();
-}
-
 function registerAutoTypeCommand() {
   // keyboard is used only for autoPlay commands
   const onType = ({ text: userInput }: { text: string }) => {
@@ -115,6 +116,7 @@ function registerAutoTypeCommand() {
   return vscode.commands.registerCommand("type", onType);
 }
 
+// This should go in the statechart:
 const enum AutoPlayState {
   Play,
   Pause,
@@ -123,6 +125,7 @@ const enum AutoPlayState {
   None
 }
 
+let autoPlaying = false;
 function runAutoPlay(autoPlayControlChannel: Channel, commandChannel: Channel) {
   go(function* () {
     let state: AutoPlayState = AutoPlayState.None;
@@ -130,6 +133,9 @@ function runAutoPlay(autoPlayControlChannel: Channel, commandChannel: Channel) {
       let result = yield alts([autoPlayControlChannel, timeout(autoPlayInterval)], { priority: true });
       if (result.channel === autoPlayControlChannel) {
         state = result.value;
+        if (state === AutoPlayState.Play) {
+          autoPlaying = true;
+        }
       } else { // timeout expired, time to do the action for my current state:
         switch (state) {
           case AutoPlayState.Play:
@@ -159,13 +165,17 @@ function startAutoPlay() {
 }
 
 export function pauseAutoPlay() {
-  putAsync(autoPlayControlChannel, AutoPlayState.Pause);
-  setManualKeyboardMode();
+  if (autoPlaying) {
+    putAsync(autoPlayControlChannel, AutoPlayState.Pause);
+    setManualKeyboardMode();
+  }
 }
 
 export function resumeAutoPlay() {
-  setAutoKeyboardMode();
-  putAsync(autoPlayControlChannel, AutoPlayState.Resume);
+  if (autoPlaying) {
+    setAutoKeyboardMode();
+    putAsync(autoPlayControlChannel, AutoPlayState.Resume);
+  }
 }
 
 // Do this when leaving the play state
@@ -209,8 +219,27 @@ function runPlay(
   // Events indicating that an edit has completed:
   const editChannel = chan(1);
 
-  let playChannel: Channel;
+  // Is there a save point at the end of the buffers?
+  const lastBuffer = buffers[buffers.length - 1];
+  if (isSavePoint(lastBuffer)) {
+    // drop it; we don't use it in playback:
+    buffers = buffers.slice(0, buffers.length - 1); // butLast
+  }
 
+  let atEndingStopPoint = () => false;
+  const stopPoints = buffers.filter(b => isStopPoint(b)).length;
+  let stopPointsPassed = 0;
+  
+  // Is there a stop point at the end of the buffers (ignoring trailing save point(s))?
+  let index = buffers.length - 1;
+  while (isSavePoint(buffers[index])) {
+    --index;
+  }
+  if (isStopPoint(buffers[index])) {
+    atEndingStopPoint = () => stopPointsPassed === stopPoints - 1;
+  }
+  
+  let playChannel: Channel;
   // If the first buffer is a save point, apply it immediately,
   // before any commands arrive:
   const firstBuffer = buffers[0];
@@ -236,16 +265,25 @@ function runPlay(
         }
       } else if (isStopPoint(playBuffer)) {
         if (command === breakoutCommand) {
+          stopPointsPassed++;
           stateService.send('RESUME_PLAY');
           playBuffer = yield playChannel;
         } else {
-          stateService.send('PLAY_PAUSED'); // We can reach here more than once; that's OK
-          // make no update to the document
-          // do not get next playBuffer
+          if (atEndingStopPoint()) {
+            stateService.send('PLAY_PAUSED_AT_END');
+            playBuffer = yield playChannel; // that should be CLOSED
+          } else {
+            stateService.send('PLAY_PAUSED'); // We can reach here more than once while at a single stop point; that's OK
+            // make no update to the document
+            // do not get next playBuffer
+          }
         }
       } else if (isSavePoint(playBuffer)) {
-        // just skip it; the plan is to have a save point at the end
-        // that I can use to skip actually playing the buffers
+        // just in case there's a save point anywhere but at the start or end of the buffers.
+        // Maybe there'll be a use for them in future.
+        // just skip it; the plan is that the save point at the end
+        // will be used to skip actually playing the buffers if desired,
+        // in order to quickly reach the end state
         playBuffer = yield playChannel;
       } else if (playBuffer === CLOSED) {
         commandChannel.close();
